76e4758ea1c4d563e9e523b728d7e236d05e504c
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-14 22:26:35 -0300

    Adicionar arquivos de projeto.

---
+++ ForTabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Digite um número da tabuada:");
+
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = i * N;
+                Console.WriteLine(i + " x " + N + " = " + resultado);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
